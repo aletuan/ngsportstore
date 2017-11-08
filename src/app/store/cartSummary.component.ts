f5c9d4bbb5cbfc9369699140689a491a3fceec80
Tuan Anh Le <anhlt1983@gmail.com>
2017-11-08 14:41:41 +0900

    add cart to the main UI

--- src/app/store/cartSummary.component.ts
+++ src/app/store/cartSummary.component.ts
@@ -2,11 +2,11 @@ import { Component } from "@angular/core";
 import { Cart } from "../model/card.model";
 
 @Component({
-  selector: "cart-summry",
+  selector: "cart-summary",
   moduleId: module.id,
   templateUrl: "cartSummary.component.html"
 })
 
 export class CartSummaryComponent {
-  constructor(public cart: Cart){};
+  constructor(public cart: Cart) { };
 }
