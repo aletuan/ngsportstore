873e2f745a3e660591df1b27b8f704562c596942
Tuan Anh Le <anhlt1983@gmail.com>
2017-11-09 08:35:34 +0900

    adding routing

--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -6,9 +6,10 @@ import { StoreComponent } from "./store.component";
 import { CartSummaryComponent } from "./cartSummary.component";
 import { CartDetailComponent } from "./cartDetail.component";
 import { CheckoutComponent } from "./checkout.component";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
-    imports: [ModelModule, BrowserModule, FormsModule],
+    imports: [ModelModule, BrowserModule, FormsModule, RouterModule],
     declarations: [StoreComponent, CartSummaryComponent, CartDetailComponent, CheckoutComponent],
     exports: [StoreComponent, CartDetailComponent, CheckoutComponent]
 })
