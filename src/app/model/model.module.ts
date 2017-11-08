ad997e06cd225879a07979e7fe5bfcb179ed4d18
Tuan Anh Le <anhlt1983@gmail.com>
2017-11-08 12:03:25 +0900

    adding comment

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from "@angular/core";
 import { ProductRepository } from "./product.repository";
 import { StaticDataSource } from "./static.datasource";
 
-@NgModule( {
+@NgModule({
   providers: [ProductRepository, StaticDataSource]
 })
 
-export class ModelModule{}
+export class ModelModule { }
+// it define a feature module
+// allow data model function (product repository) can be used everywhere in app
