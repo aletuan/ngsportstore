d57bff5c317f08469e617c50e743415e62cdc353
Tuan Anh Le <anhlt1983@gmail.com>
2017-11-08 10:42:22 +0900

    adding category description

--- src/app/model/product.model.ts
+++ src/app/model/product.model.ts
@@ -2,8 +2,8 @@ export class Product {
   constructor(
     public id?: number,
     public name?: string,
-    public description?: string,
     public category?: string,
+    public description?: string,
     public price?: number
-  ){}
+  ) { }
 }
